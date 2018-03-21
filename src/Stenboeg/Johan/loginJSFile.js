ca0df382e3908a477413872e9d35667addbcde13
Captain <mads130f@edu.easj.dk>
2018-03-21 10:14:49 +0100

    har funcs til at enter values samt klikke på knappen

---
+++ src/Stenboeg/Johan/loginJSFile.js
@@ -0,0 +1,11 @@
+// ctrl + U
+
+//Enter UserName
+document.getElementById("username").value = "Enter Username here";
+
+//Enter Password
+document.getElementById("password").value = "Enter Password here";
+
+//Run submitAction();
+submitAction();
+
