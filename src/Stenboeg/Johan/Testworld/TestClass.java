c5c970dfe37d8245497d67262d4f09d9ade7ca05
Captain <mads130f@edu.easj.dk>
2018-03-21 14:29:58 +0100

    ok k

--- src/Stenboeg/Johan/Testworld/TestClass.java
+++ src/Stenboeg/Johan/Testworld/TestClass.java
@@ -1,5 +1,8 @@
 package Stenboeg.Johan.Testworld;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
@@ -7,15 +10,24 @@ import java.net.URISyntaxException;
 
 public class TestClass {
     public static void main(String[] args) {
+        ScriptEngineManager factory = new ScriptEngineManager();
+        ScriptEngine engine = factory.getEngineByName("JavaScript");
+
+
+
+
+
         if (Desktop.isDesktopSupported()) {
             try {
                 Desktop.getDesktop().browse(new URI("https://hotspot.sszcloud.dk/fs/customwebauth/easjlogin.html"));
+
             } catch (IOException e) {
                 e.printStackTrace();
                 System.out.println("Something went wrong;D");
             } catch (URISyntaxException e) {
                 e.printStackTrace();
             }
+
         }
     }
 }
