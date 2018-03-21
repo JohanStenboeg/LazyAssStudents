7e094c73597c2f951cb92aca68eabc086e06ee28
Johan <Joha6163@edu.easj.dk>
2018-03-21 09:58:54 +0100

    Made Main + link

--- src/Stenboeg/Johan/Main.java
+++ src/Stenboeg/Johan/Main.java
@@ -1,4 +1,22 @@
 package Stenboeg.Johan;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class Main {
+    public static void main( String[] args ) {
+        if (Desktop.isDesktopSupported()){
+            try {
+                Desktop.getDesktop().browse(new URI("https://hotspot.sszcloud.dk/fs/customwebauth/easjlogin.html"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
 }
